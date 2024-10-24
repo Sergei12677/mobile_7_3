737db2f2fb96076f5f7b75faec1c596a2f65beff
Sergei12677 <gorod_26@icloud.com>
2024-10-24 22:35:36 +0500

    Add files via upload

---
+++ mobile_7_3.py..py
@@ -0,0 +1,23 @@
+from traceback import print_exc
+
+#grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+#students = {'Johnny','bildo','Steve','Khendrik','Aaron'}
+#phone_book ={"Johnny":20,"bildo":(3,667),"Steve":4,'Khendrik':(2,25),'Aaron':4}
+#print(phone_book)
+
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny','bildo','Steve','Khendrik','Aaron'}
+sorted_students = sorted (students)
+print(sorted_students)
+Aaron = 5,3,3,5,4
+print(sum(Aaron)/len(Aaron))
+Johnny = 2,2,2,3
+print(sum(Johnny)/ len(Johnny))
+Khendrik = 4,5,5,2
+print(sum(Khendrik)/ len(Khendrik))
+Steve = 4,4,3
+print(sum(Steve)/ len(Steve))
+bildo = 5,5,5,4,5
+print(sum(bildo)/ len(bildo))
+my_dict = {'Aaron':4.0, 'Johnny':2.25,'Khendrik':4.0,'Steve':3.6666666666666665,'bildo':4.8}
+print(my_dict)
